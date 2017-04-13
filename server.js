f275c25cdda6617993d000ce88a766889156c587
Eric Sowell <eric.sowell@gmail.com>
2017-04-13 07:18:33 -0500

    added link to cpsc site

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/api/data', function(req, res) {
 });
 
 
-var port = process.env.PORT;
+var port = process.env.PORT || 8765;
 
 app.listen(port, function() {
   console.log('Listening on port 8765.');
